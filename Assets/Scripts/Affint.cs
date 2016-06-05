01dbd6cdaadefff60dc647a5e44562794556c58b
Jerzy Kowalski <jerzykowalski1993@gmail.com>
2016-06-05 13:49:51 +0200

    Affint model.
    -Need to callibrate model when testing
    -Need to implement bluetooth connection

---
+++ Assets/Scripts/Affint.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using UnityEngine;
+
+public class Affint {
+    private const int PULSE_HIGH = 90;
+    private const int PULSE_LOW = 70;
+
+    private const int KILL_SPEED_HIGH = 3;
+    private const int KILL_SPEED_LOW = 2;
+
+    private const int INTERVAL = 15;
+
+    private const int HISTORY_LENGTH = 10;
+    
+    public static float ZombieSpeedFactor = 1.0f;
+    public static int EmotionalStatePoints = 0;
+    private static int Pulse = 80;
+
+    private static int zombieKilled = Oncollision.zombieKilled;
+    private static int zombieKilledInInterval = 0;
+    private static bool EmotionalStateChanged = false;
+
+    private static Timer timer = new Timer();
+
+
+    public static void Start() {
+        timer.Interval = INTERVAL * 1000;
+        timer.Elapsed += new ElapsedEventHandler(TimerElapsed);
+        timer.Start();
+    }
+
+    public static void Stop() {
+        timer.Stop();
+    }
+
+    private static void TimerElapsed(object sender, ElapsedEventArgs e) {
+        Debug.Log("Timer callback started");
+        zombieKilledInInterval = Oncollision.zombieKilled - zombieKilled;
+        Debug.Log("Killed " + zombieKilledInInterval + " zombies in last 5 seconds");
+
+        if (zombieKilledInInterval > KILL_SPEED_HIGH) {
+            EmotionalStatePoints++;
+            EmotionalStateChanged = true;
+        } else if (zombieKilledInInterval < KILL_SPEED_LOW) {
+            EmotionalStatePoints--;
+            EmotionalStateChanged = true;
+        }
+
+        if (EmotionalStateChanged) {
+            //TODO Implement pulse read
+            if (EmotionalStatePoints > 0 &&
+                 Pulse > PULSE_LOW && Pulse < PULSE_HIGH) {
+                EmotionalStatePoints++;
+            } else if (EmotionalStatePoints < 0 &&
+                Pulse > PULSE_HIGH) {
+                EmotionalStatePoints--;
+            }
+        }
+        Debug.Log("ESP: " + EmotionalStatePoints);
+
+        ZombieSpeedFactor = 1 + EmotionalStatePoints / 10;
+        if (ZombieSpeedFactor < 0.1) {
+            ZombieSpeedFactor = 0.1f;
+        }
+
+        Debug.Log("ZSF: " + ZombieSpeedFactor);
+
+        zombieKilled = Oncollision.zombieKilled;
+    }
+
+}
